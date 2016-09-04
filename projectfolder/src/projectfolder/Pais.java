5616615d103431ec70eca9787807fcc890dc428c
Danilo Favato <dfavato@gmail.com>
2016-09-04 19:24:14 -0300

    Criada classe pais

---
+++ projectfolder/src/projectfolder/Pais.java
@@ -0,0 +1,51 @@
+package projectfolder;
+
+
+public class Pais {
+	private int id;
+	private String nome;
+	private int ouro;
+	private int prata;
+	private int bronze;
+	
+	public Pais(int id, String nome) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		setOuro(0);
+		setPrata(0);
+		setBronze(0);
+	}
+	
+	public int getOuro() {
+		return ouro;
+	}
+	public void setOuro(int ouro) {
+		this.ouro = ouro;
+	}
+	public int getPrata() {
+		return prata;
+	}
+	public void setPrata(int prata) {
+		this.prata = prata;
+	}
+	public int getBronze() {
+		return bronze;
+	}
+	public void setBronze(int bronze) {
+		this.bronze = bronze;
+	}
+	public int getId() {
+		return id;
+	}
+	public String getNome() {
+		return nome;
+	}
+	
+	@Override
+	public String toString() {
+		return this.getNome() + " " + this.getOuro() + " " +
+				this.getPrata() + " " + this.getBronze();
+	}
+	
+}
