e8653acfc9ccbb0d682f442d4a0921dca2e9cdd9
Danilo Favato <dfavato@gmail.com>
2016-09-07 17:30:56 -0300

    passando metodos static para classes Pais

--- projectfolder/src/projectfolder/Pais.java
+++ projectfolder/src/projectfolder/Pais.java
@@ -1,5 +1,11 @@
 package projectfolder;
 
+import java.io.BufferedReader;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.HashMap;
+
 public class Pais {
 	private int id;
 	private String nome;
@@ -7,6 +13,52 @@ public class Pais {
 	private int prata;
 	private int bronze;
 	
+	private static HashMap<Integer, Pais> inscritos = new HashMap<>();
+	
+//	Class methods
+	public static Pais getPaisById(int id) {
+		for (Pais pais : inscritos.values()) {
+			if(pais.getId() == id) {
+				return pais;
+			}
+		}
+		return null;
+	}
+	
+	public static void registrarFromFile(String file) {
+		BufferedReader br;
+		String line;
+		try {
+			br = new BufferedReader(
+					new InputStreamReader(
+							new FileInputStream(file)));
+			do {
+				line = br.readLine();
+				if (line != null) {
+					registrarPais(new Pais(line));
+				}
+			} while (line != null);
+			br.close();
+		} catch (IOException e) {
+			System.err.println("Não foi possível abrir o arquivo " + file);
+		}
+	}
+	
+	public static void registrarPais(Pais pais) {
+		if(!inscritos.containsKey(pais.getId())) {
+			inscritos.put(pais.getId(), pais);
+		}
+	}
+	
+	public static void quadroMedalhas() {
+		System.out.println("Quadro de medalhas\n");
+		System.out.println("País Ouro Prata Bronze");
+		for (Pais pais : inscritos.values()) {
+			System.out.println(pais.toString());
+		}
+	}
+	
+//	Instance methods
 	public Pais(int id, String nome) {
 		this.id = id;
 		this.nome = nome;
--- projectfolder/src/projectfolder/Paises.java
+++ projectfolder/src/projectfolder/Paises.java
@@ -35,7 +35,6 @@ public class Paises {
 		} catch (IOException e) {
 			System.err.println("Não foi possível abrir o arquivo " + file);
 		}
-		
 	}
 	
 	public void registrarPais(Pais pais) {
