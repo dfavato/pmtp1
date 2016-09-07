a70465901b55de7e74d4933db88a435f17bbcbd5
Danilo Favato <dfavato@gmail.com>
2016-09-07 16:33:05 -0300

    removido erro 'static'

--- projectfolder/src/projectfolder/Pais.java
+++ projectfolder/src/projectfolder/Pais.java
@@ -1,9 +1,6 @@
 package projectfolder;
 
-import java.io.Serializable;
-
-public class Pais implements Serializable {
-	private static final long serialVersionUID = 1L;
+public class Pais {
 	private int id;
 	private String nome;
 	private int ouro;
