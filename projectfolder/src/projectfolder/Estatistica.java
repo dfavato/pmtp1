bc7c2a687acc0da946d950156122722fcf650d5e
Danilo Favato <dfavato@gmail.com>
2016-09-17 11:22:51 -0300

    Classe Estatistica criada para lidar com a impressão de saída

--- projectfolder/src/projectfolder/Estatistica.java
+++ projectfolder/src/projectfolder/Estatistica.java
@@ -1,31 +1,45 @@
 package projectfolder;
 
 import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class Estatistica {
-	private int type;
-	private AbstractSport sport;
 	
-	public Estatistica(int type, AbstractSport sport) {
-		this.type = type;
-		this.sport = sport;
+	public static void print(AbstractSport sport) {
+		PrintWriter pw = new PrintWriter(createOutputFile(sport));
+		pw.println(sport.toString());
 	}
 	
-	public Estatistica(int type, int idSport, SportContainer sports) {
-		this(type, sports.getSportById(idSport));
+	public static void print(CountryContainer countries) {
+		PrintWriter pw = new PrintWriter(createOutputFile());
+		pw.println(countries.toString());
 	}
 	
-	public int getType() {
-		return this.type;
-	}
-	
-	public AbstractSport getSport() {
-		return this.sport;
-	}
-	
-	private FileOutputStream createOutputFile() {
+	private static FileOutputStream createOutputFile(AbstractSport sport) {
 		FileOutputStream file;
-		file = new FileOutputStream("estatistica-" + this.getType() + "-" + this.getSport().getId());
+		String fileName;
+		fileName = "estatistica-1-" + sport.getId() + ".txt"; 
+		try {
+			file = new FileOutputStream(fileName);
+			return file;
+		} catch (IOException e) {
+			System.err.println("Não foi possível criar o arquivo.");
+			return null;
+		}	
+	}
+	
+	private static FileOutputStream createOutputFile() {
+		FileOutputStream file;
+		String fileName;
+		fileName = "estatistica-2.txt"; 
+		try {
+			file = new FileOutputStream(fileName);
+			return file;
+		} catch (IOException e) {
+			System.err.println("Não foi possível criar o arquivo.");
+			return null;
+		}	
 	}
 
 }
