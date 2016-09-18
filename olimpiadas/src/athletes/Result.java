77dc55de60ad50be4f8f3c67c58c418d0979a820
Danilo Favato <dfavato@gmail.com>
2016-09-18 16:49:25 -0300

    Voltei atrás com o método getAthletesResults, deixei na classe User mesmo

--- olimpiadas/src/athletes/Result.java
+++ olimpiadas/src/athletes/Result.java
@@ -1,11 +1,5 @@
 package athletes;
 
-import java.io.BufferedReader;
-
-import main.Util;
-import sports.AbstractSport;
-import sports.SportContainer;
-
 public class Result implements Comparable<Result>{
 	private Athlete athlete;
 	private double score;
@@ -15,32 +9,7 @@ public class Result implements Comparable<Result>{
 		this.score = score;
 	}
 	
-	public static void getAthletesResults(CountryContainer countries, SportContainer sports, String fileAddress){
-		Athlete athlete;
-		BufferedReader cursor = Util.getReader(fileAddress);
-		String line, name, results;
-		int idAthlete, idCountry, idSport;
-		Country country;
-		AbstractSport sport;
-		double scores[];
-		do {
-			line = Util.getLine(cursor);
-			if(line != null) {
-				idAthlete = Integer.parseInt(line.split(";")[0]);
-				idCountry = Integer.parseInt(line.split(";")[1]);
-				idSport = Integer.parseInt(line.split(";")[2]);
-				name = line.split(";")[3];
-				results = line.split(";")[4];
-				sport = sports.getSportById(idSport);
-				country = countries.getCountryById(idCountry);
-				athlete = new Athlete(idAthlete, name, country);
-				scores = parseResults(results.split("\\|"));
-				sport.insertCompetitorResult(athlete, scores);
-			}
-		} while (line != null);
-	}
-	
-	private static double[] parseResults(String[] line) {
+	public static double[] parseResults(String[] line) {
 		int i = 0;
 		double results[] = new double[line.length];
 		for (String s : line) {
--- olimpiadas/src/main/User.java
+++ olimpiadas/src/main/User.java
@@ -1,6 +1,11 @@
 package main;
 
+import java.io.BufferedReader;
+
+import athletes.Athlete;
+import athletes.Country;
 import athletes.CountryContainer;
+import sports.AbstractSport;
 import sports.SportContainer;
 import athletes.Result;
 
@@ -10,8 +15,35 @@ public class User {
 	public static void main(String[] args) {
 		SportContainer sports = new SportContainer("esportes.txt");
 		CountryContainer countries = new CountryContainer("paises.txt");
-		Result.getAthletesResults(countries, sports, "atletas.txt");
+		getAthletesResults(countries, sports, "atletas.txt");
 		sports.awardMedals();
 		Estatistica.printAll("estatisticas.txt", sports, countries);
+		System.out.println("Arquivos gerados com sucesso.");
+	}
+	
+	public static void getAthletesResults(CountryContainer countries, SportContainer sports, String fileAddress){
+		Athlete athlete;
+		BufferedReader cursor = Util.getReader(fileAddress);
+		String line, name, results;
+		int idAthlete, idCountry, idSport;
+		Country country;
+		AbstractSport sport;
+		double scores[];
+		do {
+			line = Util.getLine(cursor);
+			if(line != null) {
+				idAthlete = Integer.parseInt(line.split(";")[0]);
+				idCountry = Integer.parseInt(line.split(";")[1]);
+				idSport = Integer.parseInt(line.split(";")[2]);
+				name = line.split(";")[3];
+				results = line.split(";")[4];
+				sport = sports.getSportById(idSport);
+				country = countries.getCountryById(idCountry);
+				athlete = new Athlete(idAthlete, name, country);
+				scores = Result.parseResults(results.split("\\|"));
+				sport.insertCompetitorResult(athlete, scores);
+			}
+		} while (line != null);
 	}
 }
+
