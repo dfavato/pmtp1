b3d087663c53d87dbe1a8dbe9655d2ade548932a
Danilo Favato <dfavato@gmail.com>
2016-09-19 22:37:37 -0300

    Acho que agora está tudo certo

--- olimpiadas/src/athletes/Result.java
+++ olimpiadas/src/athletes/Result.java
@@ -68,7 +68,7 @@ public class Result implements Comparable<Result>{
 	 */
 	@Override
 	public String toString() {
-		return String.format("%-10s %.2f\n", this.getAthlete().getName(), this.getScore());
+		return String.format("%-10s %.2f", this.getAthlete().getName(), this.getScore());
 	}
 	
 	
